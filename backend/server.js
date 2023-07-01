e59d906402991f890b86234bd64ab77b9137c2ca
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-06-30 22:14:36 -0400

    i think news and list works when first starting

--- backend/server.js
+++ backend/server.js
@@ -33,19 +33,19 @@ app.get('/free-agents', async (req, res, next) => {
     res.json(freeAgentsData);
   } catch (error) {
     console.error('Error occurred while fetching free agents', error);
-    next(new RequestError('Failed to retrieve free agents', 500));
+    // next(new RequestError('Failed to retrieve free agents', 500));
   }
 });
 
 app.get('/transactions', async (req, res, next) => {
   try {
     const api_key = '4qtutsebhny3zgp3jadur9n6';
-    const response = await axios.get(`http://api.sportradar.us/nba/trial/v8/en/league/2023/06/29/transfers.json?api_key=${api_key}`);
+    const response = await axios.get(`http://api.sportradar.us/nba/trial/v8/en/league/2023/06/30/transfers.json?api_key=${api_key}`);
     const transactionsData = response.data;
     res.json(transactionsData);
   } catch (error) {
     console.error('Error occurred while fetching transactions', error);
-    next(new RequestError('Failed to retrieve transactions', 500));
+    // next(new RequestError('Failed to retrieve transactions', 500));
   }
 });
 
