2919824582a3217d8124895f55e8ee117a358e92
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-07-02 23:37:42 -0400

    trying to add tweets

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const winston = require('winston');
-require('dotenv').config();
-
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const winston = require("winston");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3001;
@@ -12,19 +11,21 @@ const PORT = 3001;
 const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: "error.log" }),
   ],
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  logger.error('An error occurred:', err);
-  res.status(err.statusCode || 500).json({ error: err.message || 'Internal Server Error' });
+  logger.error("An error occurred:", err);
+  res
+    .status(err.statusCode || 500)
+    .json({ error: err.message || "Internal Server Error" });
 });
 
 app.use(cors());
 
-app.get('/free-agents', async (req, res, next) => {
+app.get("/free-agents", async (req, res, next) => {
   try {
     const api_key = process.env.API_KEY;
     const apiUrl = `https://api.sportradar.com/nba/trial/v8/en/league/free_agents.json?api_key=${api_key}`;
@@ -34,25 +35,36 @@ app.get('/free-agents', async (req, res, next) => {
 
     res.json(freeAgentsData);
   } catch (error) {
-    console.error('Error occurred while fetching free agents', error);
+    console.error("Error occurred while fetching free agents", error);
     // next(new RequestError('Failed to retrieve free agents', 500));
   }
 });
 
-app.get('/transactions', async (req, res, next) => {
+app.get("/transactions", async (req, res, next) => {
   try {
     const api_key = process.env.API_KEY;
-    const {date} = req.query;
-    const formattedDate = date.replace(/-/g, "/")
+    const { date } = req.query;
+    const formattedDate = date.replace(/-/g, "/");
     // const response = await axios.get(`http://api.sportradar.us/nba/trial/v8/en/league/2023/06/29/transfers.json?api_key=${api_key}`);
-    const response = await axios.get(`http://api.sportradar.us/nba/trial/v8/en/league/${formattedDate}/transfers.json?api_key=${api_key}`);
+    const response = await axios.get(
+      `http://api.sportradar.us/nba/trial/v8/en/league/${formattedDate}/transfers.json?api_key=${api_key}`
+    );
 
     const transactionsData = response.data;
     res.json(transactionsData);
   } catch (error) {
-    console.error('Error occurred while fetching transactions', error);
+    console.error("Error occurred while fetching transactions", error);
     // next(new RequestError('Failed to retrieve transactions', 500));
   }
+});
+
+app.get("/tweets", async (rec, res, next) => {
+  const api_key = process.env.TWITTER_API_KEY;
+  const api_secret_key = process.env.TWITTER_API_SECRET_KEY;
+  const access_token = process.env.TWITTER_ACCESS_TOKEN;
+  const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+  const userName = "ShamsCharania";
+  const apiUrl = `https://api.twitter.com/2/users/by/username/${userName}`;
 });
 
 app.listen(PORT, () => {
