2da5b7d810fe6e6443fe62b6479b652ffc298ed3
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2023-07-03 17:41:41 -0400

    first attempt

--- backend/server.js
+++ backend/server.js
@@ -59,12 +59,46 @@ app.get("/transactions", async (req, res, next) => {
 });
 
 app.get("/tweets", async (rec, res, next) => {
-  const api_key = process.env.TWITTER_API_KEY;
-  const api_secret_key = process.env.TWITTER_API_SECRET_KEY;
-  const access_token = process.env.TWITTER_ACCESS_TOKEN;
-  const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
-  const userName = "ShamsCharania";
-  const apiUrl = `https://api.twitter.com/2/users/by/username/${userName}`;
+  try {
+    const api_key = process.env.TWITTER_API_KEY;
+    const api_secret_key = process.env.TWITTER_API_SECRET_KEY;
+    const access_token = process.env.TWITTER_ACCESS_TOKEN;
+    const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+    const userNames = ["ShamsCharania", "wojespn"];
+    const tweets = [];
+
+    for (const userName of userNames) {
+      // Get user ID
+      const userIdResponse = await axios.get(
+        `https://api.twitter.com/2/users/by/username/${userName}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      const userId = userIdResponse.data.data.id;
+
+      // Get recent tweets
+      const tweetsResponse = await axios.get(
+        `https://api.twitter.com/2/users/${userId}/tweets`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      tweets.push(...tweetsResponse.data.data);
+    }
+
+    res.json(tweets);
+  } catch (error) {
+    console.error("Error occurred while fetching tweets", error);
+  }
+
+
 });
 
 app.listen(PORT, () => {
--- frontend/src/pages/Tweets.js
+++ frontend/src/pages/Tweets.js
@@ -1,7 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const Tweets = () => {
-    return (
-        <h1>Tweets from a few goats in the NBA world</h1>
-    )
-}
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    const fetchTweets = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/tweets');
+        setTweets(response.data);
+      } catch (error) {
+        console.error('Error occurred while fetching tweets:', error);
+      }
+    };
+
+    fetchTweets();
+  }, []);
+
+  return (
+    <div>
+      <h2>Recent Tweets</h2>
+      <ul>
+        {tweets.map(tweet => (
+          <li key={tweet.id}>{tweet.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Tweets;
